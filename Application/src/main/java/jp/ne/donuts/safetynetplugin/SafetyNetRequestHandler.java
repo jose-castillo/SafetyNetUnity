64c71348cedcd85933040e7da0ce6cf0146d0bd8
Jose Castillo <jose.castillo@donuts.ne.jp>
2017-09-05 13:22:34 +0900

    Git Ignore and other changes

--- Application/src/main/java/jp/ne/donuts/safetynetplugin/SafetyNetRequestHandler.java
+++ Application/src/main/java/jp/ne/donuts/safetynetplugin/SafetyNetRequestHandler.java
@@ -1,26 +1,11 @@
 package jp.ne.donuts.safetynetplugin;
 
-import android.app.Activity;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 
 import com.unity3d.player.UnityPlayer;
 
 public class SafetyNetRequestHandler {
-
-    //Unity側のこのインタフェースから派生クラスが作成されます
-    public static interface IListener {
-        public void onComplete(boolean status);
-    }
-
-    private static Activity mActivity;
-    private IListener mListener;
-
-    public static SafetyNetRequestHandler mListner = null;
-
-    public static SafetyNetRequestHandler instance() {
-        return mListner;
-    }
 
     public static void RequestVerification(final byte[] nonce, final String key, String callbackGameObject, String callbackMethod) {
 
@@ -31,17 +16,10 @@ public class SafetyNetRequestHandler {
         safetynetExec.key            = key;
         safetynetExec.nonceFromUnity = nonce;
 
-
-        mActivity = UnityPlayer.currentActivity;
         //フラグメントを作成し、アクティビティに追加します。
-        FragmentManager fm = mActivity.getFragmentManager();
+        FragmentManager fm     = UnityPlayer.currentActivity.getFragmentManager();
         FragmentTransaction ft = fm.beginTransaction();
         ft.add(safetynetExec, "TAG");
         ft.commit();
-    }
-
-    // このメソッドが呼び出されるとリスナーに通知する
-    public void onComplete(boolean v) {
-        mListener.onComplete(v);
     }
 }
